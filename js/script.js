859cef3a0aa62b5edccc8a0f296e864173f29f86
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-12 14:25:18 +0200

    add array(teamMembers)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,49 @@
 //check console log
 console.log('ok js');
+
+//richiamo elementi dal DOM
+const mainElement = document.getElementById('main-content');
+
+
+// creo un Array contenente degli oggetti
+const TeamMembers = 
+[
+    {
+        immagine : 'img/wayne-barnett-founder-ceo.jpg',
+        nome : 'WayneBarnett',
+        occupazione : 'Founder & CEO'
+    },
+
+    {
+        immagine : 'angela-caroll-chief-editor.jpg',
+        nome : 'Angela Caroll',
+        occupazione : 'Chief Editor'
+    },
+
+    {
+        immagine : 'walter-gordon-office-manager.jpg',
+        nome : 'Walter Gordon',
+        occupazione : '	Office Manager  '
+    },
+
+    {
+        immagine : 'angela-lopez-social-media-manager.jpg',
+        nome : 'Angela Lopez ',
+        occupazione : 'Social Media Manager'
+    },
+
+    {
+        immagine : 'scott-estrada-developer.jpg',
+        nome : 'Scott Estrada',
+        occupazione : 'Developer'
+    },
+
+    {
+        immagine : 'barbara-ramos-graphic-designer.jpg',
+        nome : 'Barbara Ramos',
+        occupazione : '	Graphic Designer'
+    }
+
+];
+
+console.table(TeamMembers);
